5de68bb94ef69f0b1d023115164b70baad3c5997
Pouya <pouyaheydary@gmail.com>
2019-04-19 17:42:24 +0430

    added toast to mainActivity

--- app/src/main/java/com/sematech/android/basic/farvardin98/MainActivity.java
+++ app/src/main/java/com/sematech/android/basic/farvardin98/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sematech.android.basic.farvardin98;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Toast.makeText(this, "asjasjkna", Toast.LENGTH_SHORT).show();
     }
 }
